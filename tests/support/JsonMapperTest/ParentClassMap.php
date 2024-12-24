8befa371e47bd2cc01a301cc48a28bf141da510c
Startail the 'Coon <startail@outlook.com>
2024-12-24 19:26:35 +0100

    Apply Parent JSON Fix

---
+++ tests/support/JsonMapperTest/ParentClassMap.php
@@ -0,0 +1,23 @@
+<?php
+class JsonMapperTest_ParentClassMap_Test {
+    /**
+     * @var JsonMapperTest_ParentClassBody_Test
+     */
+    public $body;
+}
+class JsonMapperTest_ParentClassBody_Test { }
+class JsonMapperTest_ProtocolClassBody_Test {
+    /**
+     * @var string
+     */
+    public $name;
+    /**
+     * @var string
+     */
+    public $version;
+    /**
+     * @var string
+     */
+    public $content;
+}
+?>
